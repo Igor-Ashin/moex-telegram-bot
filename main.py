26d8276b66d4a8e62dbdcbfc3133ac2d786329be
Igor <igor-post92@mail.ru>
2025-07-02 13:50:46 +0300

    Update main.py
    upd2

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def long_obv(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = "📉 OBV растет, а цена падает (за 2 недели):\n\n"
     for ticker, price_delta, obv_delta in result:
         msg += f"{ticker}: Цена {price_delta}%, OBV +{obv_delta}%
-""
+"
     await update.message.reply_text(msg)
 
 
